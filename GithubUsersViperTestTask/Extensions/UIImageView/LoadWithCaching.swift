2c39a8bfea94cdebe313b7b3f928367153b73671
Eriksoon-dev <grimebusiness@yandex.ru>
2021-02-14 09:41:10 -0800

    move image loading outside the main thread

--- GithubUsersViperTestTask/Extensions/UIImageView/LoadWithCaching.swift
+++ GithubUsersViperTestTask/Extensions/UIImageView/LoadWithCaching.swift
@@ -4,23 +4,30 @@ import Alamofire
 extension UIImageView {
     // load images with caching in URLSession
     func loadWithCaching(urlString: String, placeholderImageName: String? = nil ) {
-        if let placeholder = placeholderImageName {
-            self.image = UIImage(named: placeholder)
-        } else {
-            self.image = nil
-        }
-        
-        guard let url = URL(string: urlString) else { return }
-        
-        let request = URLRequest(url: url, cachePolicy: .returnCacheDataElseLoad, timeoutInterval: 10)
-        
-        URLSession.shared.dataTask(with: request) { [weak self] (data, _, error) in
-            guard let data = data, let image = UIImage(data: data) else { return }
-            
+        // make downloading images out of main thread
+        DispatchQueue.global().async {
+            // update ui in main thread
             DispatchQueue.main.async {
-                self?.image = image
+                if let placeholder = placeholderImageName {
+                    self.image = UIImage(named: placeholder)
+                } else {
+                    self.image = nil
+                }
             }
             
-        }.resume()
+            guard let url = URL(string: urlString) else { return }
+            
+            let request = URLRequest(url: url, cachePolicy: .returnCacheDataElseLoad, timeoutInterval: 10)
+            
+            URLSession.shared.dataTask(with: request) { [weak self] (data, _, error) in
+                guard let data = data, let image = UIImage(data: data) else { return }
+                // update ui in main thread
+                DispatchQueue.main.async {
+                    self?.image = image
+                }
+                
+            }.resume()
+        }
+        
     }
 }
